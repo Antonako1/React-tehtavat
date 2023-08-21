157d3a9b9402bf5e9c40a91a9b521690ccf74aca
atlol <114558368+atlol@users.noreply.github.com>
2023-08-21 11:18:30 +0300

    1.7 valmis

--- Reactin perusteet 1/src/App.jsx
+++ Reactin perusteet 1/src/App.jsx
@@ -10,13 +10,30 @@ const Buttons = (props) => {
     </div>
   )
 }
+
+const calculateAverage = (good, neutral, bad, all) => {  
+  const average = (good + 0 + -bad) / all;
+
+  return average;
+};
+
+
 const Statistics = (props) => {
+  /*
+  props.all  | 100%
+  props.good | x%
+  */
+  let positive = (props.good * 100) / props.all
   return(
     <div>
       <h1>Statistics</h1>
       <p>Good: {props.good} </p>
       <p>Neutral: {props.neutral} </p>
       <p>Bad: {props.bad} </p>
+      <p>All: {props.all}</p>
+      <hr />
+      <p>Average: {calculateAverage(props.good, props.neutral, props.bad, props.all)}</p>
+      <p>Positive: {positive}%</p>
     </div>
   )
 }
@@ -24,17 +41,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
+    handleAll(1)
   }
   const handleNeutral=()=>{
     setNeutral(neutral + 1)
+    handleAll(1)
   }
   const handleBad=()=>{
     setBad(bad + 1)
+    handleAll(1)
   }
-
+  const handleAll = (parametri) =>{
+    console.log(parametri)
+    setAll(all + parametri)
+  }
   return (
     <div>
       <Buttons 
@@ -46,6 +70,7 @@ const App = () => {
         good={good}
         neutral={neutral}
         bad={bad}
+        all={all}
       />
     </div>
   )
