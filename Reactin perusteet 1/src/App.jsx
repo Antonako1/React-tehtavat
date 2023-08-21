f65c9abd3c862e14361b7542229d2db67ced4688
atlol <114558368+atlol@users.noreply.github.com>
2023-08-21 09:53:34 +0300

    1.6 valmis

--- Reactin perusteet 1/src/App.jsx
+++ Reactin perusteet 1/src/App.jsx
@@ -1,62 +1,51 @@
 import { useState } from 'react'
 
-const Header = (props) => {
-  return <h1>{props.course.name}H</h1>
-}
-
-const Part = (props) => {
-  return <p>{props.part} {props.exercises} </p>
-}
-
-const Content = (props) => {
-  return(
+const Buttons = (props) => {
+  return (
     <div>
-      <Part part={props.part1.parts[0].name} exercises={props.part1.parts[0].exercises} />
-      <Part part={props.part2.parts[1].name} exercises={props.part2.parts[1].exercises} />
-      <Part part={props.part3.parts[2].name} exercises={props.part3.parts[2].exercises} />
+      <h1>Give feedback</h1>
+      <button onClick={props.handleGood}>Good</button>
+      <button onClick={props.handleNeutral}>Neutral</button>
+      <button onClick={props.handleBad}>Bad</button>
     </div>
   )
 }
-const Total = (props) => {
-  console.log(props)
-  return <p >Number of exercises {
-    props.part1.parts[0].exercises + 
-    props.part2.parts[1].exercises + 
-    props.part3.parts[2].exercises
-    }</p>
+const Statistics = (props) => {
+  return(
+    <div>
+      <h1>Statistics</h1>
+      <p>Good: {props.good} </p>
+      <p>Neutral: {props.neutral} </p>
+      <p>Bad: {props.bad} </p>
+    </div>
+  )
 }
-
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+  const handleNeutral=()=>{
+    setNeutral(neutral + 1)
+  }
+  const handleBad=()=>{
+    setBad(bad + 1)
   }
 
   return (
     <div>
-      <Header course={course} />
-      <Content 
-      part1={course} 
-      part2={course} 
-      part3={course}
+      <Buttons 
+      handleGood={handleGood} 
+      handleNeutral={handleNeutral} 
+      handleBad={handleBad} 
       />
-      <Total
-      part1={course} 
-      part2={course} 
-      part3={course}  
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
       />
     </div>
   )
