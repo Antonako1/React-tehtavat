9e0829abdd0f1503a20005818922b2e60a3bf521
antako <114558368+Antonako1@users.noreply.github.com>
2023-08-21 14:18:48 +0300

    1.12 valmis

--- Reactin perusteet 1/src/App.jsx
+++ Reactin perusteet 1/src/App.jsx
@@ -1,111 +1,28 @@
 import { useState } from 'react'
 
-/*-----------------------------------------------------------
-                    Button elements
-*/
-const ButtonElem = (props) => {
-    return <button onClick={props.handleEvent}>{props.text}</button>
-}
-
-const Buttons = (props) => {
-  return (
-    <thead>
-      <tr>
-        <th>Give feedback</th>
-      </tr>
-      <tr>
-        <td>
-          <ButtonElem handleEvent={props.handleGood} text={"Good"} />
-          <ButtonElem handleEvent={props.handleNeutral} text={"Neutral"} />
-          <ButtonElem handleEvent={props.handleBad} text={"Bad"} />
-        </td>
-      </tr>
-    </thead>
-  )
-}
-/*-----------------------------------------------------------
-                      Average
-*/
-const calculateAverage = (good, neutral, bad, all) => {  
-  const average = (good + 0 + -bad) / all;
-  return average;
-};
-
-const StatisticsLine = (props) => {
-  return <tr><td>{props.text} {props.value}</td></tr>
-}
-
-const Statistics = (props) => {
-  let average, positive = undefined;
-  average = calculateAverage(props.good, props.neutral, props.bad, props.all)
-  positive = (props.good * 100) / props.all
-  if(props.good === 0 && props.neutral === 0 && props.bad === 0){
-    return(
-      <tbody>
-        <tr>
-          <th>Statistics</th>
-        </tr>
-        <tr>
-          <td>No feedback given</td>
-        </tr>
-      </tbody>
-    )
-  } else{
-    return(
-      <tbody>
-        <tr>
-          <th>Statistics</th>
-        </tr>
-        <StatisticsLine text="Good:" value={props.good} />
-        <StatisticsLine text="Neutral:" value={props.neutral} />
-        <StatisticsLine text="Bad:" value={props.bad} />
-        <StatisticsLine text="All:" value={props.all} />
-        <StatisticsLine text={"Average:"} value={average} />
-        <StatisticsLine text={"Positive:"} value={positive} />
-      </tbody>
-    )
-  }
-}
-/*----------------------------------------------------------------------
-                      App element
-*/
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const handleGood = () => {
-    setGood(good + 1)
-    handleAll(1)
-  }
-  const handleNeutral=()=>{
-    setNeutral(neutral + 1)
-    handleAll(1)
-  }
-  const handleBad=()=>{
-    setBad(bad + 1)
-    handleAll(1)
-  }
-  const handleAll = (parametri) =>{
-    console.log(parametri)
-    setAll(all + parametri)
+  const [selected, setSelected] = useState(0)
+  const handleSelect = () => {
+    let randomNumber = Math.floor((Math.random() * anecdotes.length) + 1);
+    setSelected(randomNumber)
   }
   return (
-    <table>
-      <Buttons
-        handleGood={handleGood}
-        handleNeutral={handleNeutral}
-        handleBad={handleBad}
-      />
-
-      <Statistics
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        all={all}
-      />
-    </table>
+    <div>
+      {anecdotes[selected]}
+      <br/>
+      <button onClick={handleSelect}>next anecdote</button>
+    </div>
   )
 }
 
