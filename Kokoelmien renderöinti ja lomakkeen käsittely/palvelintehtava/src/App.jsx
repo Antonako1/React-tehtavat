39303976a58ef5fe3e48c5422502b9ef4afa70af
antako <114558368+Antonako1@users.noreply.github.com>
2023-08-28 09:14:26 +0300

    2.4 valmis

--- Kokoelmien renderöinti ja lomakkeen käsittely/palvelintehtava/src/App.jsx
+++ Kokoelmien renderöinti ja lomakkeen käsittely/palvelintehtava/src/App.jsx
@@ -12,60 +12,108 @@ const Content = (props) => {
   return(
     <div>
       {
-        props.course.parts.map(part => <Part part={part.name} exercises={part.exercises} />)
+        props.course.parts.map(part => <Part part={part.name} exercises={part.exercises} key={part.name}/>)
       }
     </div>
   )
 } 
 
 const Total = (props) => {
-  console.log(props)
   return (
-		<p>Number of exercises  
-		  {
-		    props.course.parts.reduce((total, part) => total + part.exercises, 0)
-		  }
-		</p>
+    <b>
+      <p>Number of exercises: {' '}
+        { 
+          props.course.parts.reduce((total, part) => total + part.exercises, 0)
+        } 
+		  </p>
+    </b>
 		)
 }
 const Course = (props) => {
-  const course = props.course;
   return(
     <div>
-      <Header course={course} />
-      <Content course={course}/>
-      <Total course={course}/>
+      {
+        props.course.map(section => 
+          <div key={section.name}>
+            <Header course={section} />
+            <Content course={section} courseLength={props.courseLength}/>
+            <Total course={section} courseLength={props.courseLength}/>
+          </div>
+        )
+      }
+      
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'State of a component aaa',
-        exercises: 142,
-        id: 4
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+    , 
+    {
+      name: 'test.js',
+      id: 3,
+      parts: [
+        {
+          name: 'test2',
+          exercises: 33,
+          id: 1
+        },
+        {
+          name: 'test22',
+          exercises: 17,
+          id: 2
+        }
+      ]
+    }
+  ]
+
+  // const [courseLength, setCourseLength] = useState(0);
+  // const pituus = courses.length;
+  // setCourseLength(pituus)
+  const courseLength = courses.length;
+  const course = courses
 
   return (
     <div>
