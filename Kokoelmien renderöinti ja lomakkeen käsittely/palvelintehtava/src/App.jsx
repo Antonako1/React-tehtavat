7b3535f0275afadc57e095da8f32992aa2435167
antako <114558368+Antonako1@users.noreply.github.com>
2023-08-29 08:33:00 +0300

    2.6 valmis

--- Kokoelmien renderöinti ja lomakkeen käsittely/palvelintehtava/src/App.jsx
+++ Kokoelmien renderöinti ja lomakkeen käsittely/palvelintehtava/src/App.jsx
@@ -11,23 +11,31 @@ const App = () => {
   }
 
   const handleSubmit = () => {
-    setPersons({name: newName})
-    console.log(persons)
+    let copy = [...persons];
+    copy.push(
+      {name: newName}
+    )
+    setPersons(copy)
+    document.getElementById('input-field').value = "";
   }
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={e => e.preventDefault()}>
         <div>
-          name: <input onChange={updateInput}/>
+          name: <input onChange={updateInput}
+                  placeholder='Nimi'
+                  id='input-field'
+                />
         </div>
         <div>
           <button type="submit" onClick={handleSubmit}>add</button>
         </div>
       </form>
-      <div>debug: {newName}</div>   
       <h2>Numbers</h2>
-      ...
+      {
+        persons.map(people => <p key={people.name}>{people.name}</p>)
+      }
     </div>
   )
 
