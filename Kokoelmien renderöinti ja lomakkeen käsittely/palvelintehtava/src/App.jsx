e0be7d2b970237f2c55d50e9cb5a1de096be458c
antako <114558368+Antonako1@users.noreply.github.com>
2023-08-29 09:44:05 +0300

    2.8 valmis

--- Kokoelmien renderöinti ja lomakkeen käsittely/palvelintehtava/src/App.jsx
+++ Kokoelmien renderöinti ja lomakkeen käsittely/palvelintehtava/src/App.jsx
@@ -1,45 +1,82 @@
 import { useState } from 'react'
 
-const checkDuplicate = (obj, copy) => {
-  for(let i = 0; i < copy.length; i++){
-    if(copy[i].name == obj.name){
-      return true;
+const checkDuplicates = (obj, copy, type) => {
+  if(type === "Name"){
+    for(let i = 0; i < copy.length; i++){
+      if(copy[i].name === obj.name){
+        return true;
+      }
+    }
+    return false
+  } else if(type === "Number") {
+    for(let i = 0; i < copy.length; i++){
+      if(copy[i].number === obj.number){
+        return true;
+      }
     }
   }
-  return false
+}
+
+const checkErrors = (objecti, copy) => {
+  let mem = 0;
+  const reg = new RegExp('^[0-9]+$');
+  if(checkDuplicates(objecti, copy, "Name")){
+    alert(`${objecti.name} is already in the phonebook`)
+    mem++;
+  }
+  if(checkDuplicates(objecti, copy, "Number")){
+    alert(`${objecti.number} is already in the phonebook`)
+    mem++;
+  }
+  if(reg.test(objecti.number)){
+    alert(`${objecti.number} is not a number`)
+    mem++;
+  }
+  return mem;
 }
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: 123456789 }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState(0)
 
-  const updateInput = (e) => {
+  const updateInputStr = (e) => {
     setNewName(e.target.value)
   }
-
+  const updateInputInt = (e) => {
+    setNewNum(e.target.value)
+  }
   const handleSubmit = () => {
     let copy = [...persons];
-    let objecti = {name: newName};
-
-    if(checkDuplicate(objecti, copy)){
-      alert(`${objecti.name} is already in phonebook`)
-    } else {
-      copy.push(objecti)
-      setPersons(copy)
+    let objecti = {
+      name: newName,
+      number: newNum
+    };
+    if(checkErrors(objecti, copy) === 0){
+        copy.push(objecti)
+        setPersons(copy)
     }
-    document.getElementById('input-field').value = "";
+    document.getElementById('input-field-str').value = "";
+    document.getElementById('input-field-int').value = "";
   }
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={e => e.preventDefault()}>
         <div>
-          name: <input onChange={updateInput}
-                  placeholder='Nimi'
-                  id='input-field'
+          name: <input onChange={updateInputStr}
+                  placeholder='Kokonimi'
+                  id='input-field-str'
                 />
+        </div>
+        <br />
+        <div>
+          number: <input onChange={updateInputInt}
+                      placeholder='Puhelin numero'
+                      id='input-field-int' 
+                    />
         </div>
         <div>
           <button type="submit" onClick={handleSubmit}>add</button>
@@ -47,7 +84,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {
-        persons.map(people => <p key={people.name}>{people.name}</p>)
+        persons.map(people => <p key={people.name}>{people.name} | {people.number}</p>)
       }
     </div>
   )
