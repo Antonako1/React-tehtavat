8a4948b912a6d7e190bb2558a49daaaba9e45f40
Akonpelto Anton <gr275806@gradia.fi>
2023-08-30 13:23:41 +0300

    2.11 valmis

--- Kokoelmien renderöinti ja lomakkeen käsittely/palvelintehtava/src/App.jsx
+++ Kokoelmien renderöinti ja lomakkeen käsittely/palvelintehtava/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-
+import { useState, useEffect  } from 'react'
+import axios from 'axios';
 const checkDuplicates = (obj, copy, type) => {
   if(type === "Name"){
     for(let i = 0; i < copy.length; i++){
@@ -110,12 +110,17 @@ const Persons = (props) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])  
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState(0);
   const [idList, setIdList] = useState([]);
@@ -165,7 +170,7 @@ const App = () => {
         />
 
         <hr />
-        
+
         <AddPersons
           updateInputStr={updateInputStr}
           updateInputInt={updateInputInt}
