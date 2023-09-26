6c08628442c7d97fc8dfaae4aa05f924a53beee2
antonakonpelto <114558368+antonakonpelto@users.noreply.github.com>
2023-09-26 11:56:43 +0300

    Update App.jsx

--- Kokoelmien renderöinti ja lomakkeen käsittely/palvelintehtava/src/App.jsx
+++ Kokoelmien renderöinti ja lomakkeen käsittely/palvelintehtava/src/App.jsx
@@ -32,7 +32,7 @@ const checkErrors = (objecti, copy) => {
   console.log(reg.test(objecti.number))
   if(reg.test(objecti.number) == false){
     alert(`Warning: ${objecti.number} is not a number`)
-    mem++;
+    return null
   }
   return mem;
 }
@@ -139,10 +139,12 @@ const App = () => {
       number: newNum,
       id: (persons.length+1)
     };
-    if(checkErrors(objecti, copy) === 0){
+    if(checkErrors(objecti, copy) === 0 && checkErrors(objecti, copy) !== null){
           objecti.number = String(objecti.number)
         copy.push(objecti)
         setPersons(copy)
+    }else {
+      return;
     }
     document.getElementById('input-field-str').value = "";
     document.getElementById('input-field-int').value = "";
